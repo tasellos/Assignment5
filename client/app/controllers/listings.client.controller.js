9483db1e39f7712adc28518459c989de65886532
tasellos <31318233+tasellos@users.noreply.github.com>
2017-09-19 20:13:18 -0400

    Add files via upload

--- client/app/controllers/listings.client.controller.js
+++ client/app/controllers/listings.client.controller.js
@@ -79,6 +79,25 @@ angular.module('listings').controller('ListingsController', ['$scope', '$locatio
         successfully finished, navigate back to the 'listing.list' state using $state.go(). If an error 
         occurs, pass it to $scope.error. 
        */
+        $scope.error = null;
+        if (!isValid) {
+            $scope.$broadcast('show-errors-check-validity', 'articleForm');
+            return false;
+        }
+
+        var listing = {
+            name: $scope.name,
+            code: $scope.code,
+            address: $scope.address
+        };
+
+
+        Listings.update($scope.listing._id, listing)
+            .then(function (response) {
+                $state.go('listings.list', { successMessage: 'Listing updated succesfully!' });
+            }, function (error) {
+                $scope.error = 'Unable to save listing!\n' + error;
+            });
     };
 
     $scope.remove = function() {
@@ -86,6 +105,12 @@ angular.module('listings').controller('ListingsController', ['$scope', '$locatio
         Implement the remove function. If the removal is successful, navigate back to 'listing.list'. Otherwise, 
         display the error. 
        */
+        Listings.delete($scope.listing._id)
+            .then(function (response) {
+                $state.go('listings.list', { successMessage: 'Listing deleted succesfully!' });
+            }, function (error) {
+                $scope.error = 'Unable to save listing!\n' + error;
+            });
     };
 
     /* Bind the success message to the scope if it exists as part of the current state */
@@ -93,13 +118,32 @@ angular.module('listings').controller('ListingsController', ['$scope', '$locatio
       $scope.success = $stateParams.successMessage;
     }
 
-    /* Map properties */
-    $scope.map = {
-      center: {
-        latitude: 29.65163059999999,
-        longitude: -82.3410518
-      }, 
-      zoom: 14
+    $scope.initialize = function () {
+        /* Map properties */
+        debugger;
+        $scope.markers = [];
+        Listings.getAll().then(function (response) {
+            $scope.listings = response.data;
+
+            for (var i in $scope.listings) {
+                if ($scope.listings[i].coordinates) {
+                    $scope.markers.push($scope.listings[i]);
+                }
+            }
+            $scope.loading = false; 
+        },
+            function (error) {
+            $scope.loading = false;
+            $scope.error = 'Unable to retrieve listings!\n' + error;
+        });
+
+        $scope.map = {
+            center: {
+                latitude: 29.65163059999999,
+                longitude: -82.3410518
+            },
+            zoom: 14
+        }
     }
   }
 ]);
